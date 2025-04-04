f0b0e9ddcfdfc3458b84b5d72fb217a8792e40cf
MunaFatinah <munafatinaha@gmail.com>
2025-04-04 11:20:05 +0700

    Update Bus.java

--- tugaskeempat/Bus.java
+++ tugaskeempat/Bus.java
@@ -1,20 +1,22 @@
-// Package declaration
-package praktic.inheritance.transportation;
+package praktic.inheritance.transportation; // package
 
-public class Bus extends Vehicle {
-    private static final int FARE_PER_STOP = 2000;
-    private int totalStops = 5;
+public class Bus extends Vehicle { // kelas Bus merupakan subclass dari vehicle 
+    private static final int FARE_PER_STOP = 2000; // tarif per pemberhentian
+    private int totalStops = 5; // jumlah total pemberhentian pada rute, bertipe data integer
     
+    // konstruktor bus yang memanggil konstruktor vehicle 
     public Bus(String name, int capacity, String route) {
         super(name, capacity, route);
     }
     
-    public void displayFareInfo() {
-        System.out.println("Total pemberhentian: " + totalStops);
-        System.out.println("Tarif per halte: Rp " + FARE_PER_STOP);
-        System.out.println("Total tarif perjalanan bus: Rp " + calculateFare());
+    // method untuk menampilkan informasi tarif bus 
+    public void displayFareInfo() { // diberi nama displayFareInfo
+        System.out.println("Total pemberhentian: " + totalStops); // mencetak total pemberhentian
+        System.out.println("Tarif per halte: Rp " + FARE_PER_STOP); // mencetak tarif per halte
+        System.out.println("Total tarif perjalanan bus: Rp " + calculateFare()); // mencetak total tarif
     }
-    
+
+    // Override method calculateFare untuk menghitung tarif berdasarkan jumlah penumpang dan pemberhentian
     @Override
     public int calculateFare() {
         return currentPassengers * totalStops * FARE_PER_STOP;
