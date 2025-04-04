9961b25489034eb1fdeefe888df718b51149aecf
MunaFatinah <munafatinaha@gmail.com>
2025-04-04 11:20:49 +0700

    Update Taxi.java

--- tugaskeempat/Taxi.java
+++ tugaskeempat/Taxi.java
@@ -1,20 +1,22 @@
-// Package declaration
-package praktic.inheritance.transportation;
+package praktic.inheritance.transportation; // package 
 
-public class Taxi extends Vehicle {
-    private static final int FARE_PER_KM = 5000;
-    private int distance = 10; // 10 km sesuai contoh output
+public class Taxi extends Vehicle { // kelas Taxi merupakan subclass dari Vehicle 
+    private static final int FARE_PER_KM = 5000; // tarif per kilometer
+    private int distance = 10; // jarak tempuh defaul taksi, 10 km sebagai contoh
     
+    // konstruktor taksi yang memanggil konstruktor vehicle
     public Taxi(String name, int capacity, String route) {
         super(name, capacity, route);
     }
     
-    public void displayFareInfo() {
-        System.out.println("Total jarak rute: " + distance + " km");
-        System.out.println("Tarif per km: Rp " + FARE_PER_KM);
-        System.out.println("Total tarif perjalanan taksi: Rp " + calculateFare());
+    // method untuk menampilkan informasi tarif taksi
+    public void displayFareInfo() { // fiberi nama displayFareInfo
+        System.out.println("Total jarak rute: " + distance + " km"); // mencetak km total jarak rute
+        System.out.println("Tarif per km: Rp " + FARE_PER_KM); // mencetak tarif per kilo
+        System.out.println("Total tarif perjalanan taksi: Rp " + calculateFare()); // mencetak total tarif
     }
     
+    // override method calculateFare untuk menghitung tarif berdasarkan jarak
     @Override
     public int calculateFare() {
         return distance * FARE_PER_KM;
