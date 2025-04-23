bc57ab2be1f0f32509b8c68dd05dd5f7a4b1df0b
MunaFatinah <munafatinaha@gmail.com>
2025-04-23 23:29:39 +0700

    Update Square.java

--- tugaskelima/Square.java
+++ tugaskelima/Square.java
@@ -1,39 +1,35 @@
-// package praktic.geometry.shapes
-package praktic.geometry.shapes;
+package praktic.geometry.shapes; // package untuk bentuk spesifik
 
-import praktic.geometry.bases.Shape;
-import praktic.geometry.interfaces.TwoDimensional;
+import praktic.geometry.bases.Shape; // mengimpor kelas Shape sebagai superclass
+import praktic.geometry.interfaces.TwoDimensional; // mengimpor interface TwoDimensional untuk bentuk 2D
 
-/**
- * Class Square untuk persegi 2D
- */
-public class Square extends Shape implements TwoDimensional {
-    private double side;
-    
-    public Square() {
-        // Constructor default
-        setName("Square");
+
+public class Square extends Shape implements TwoDimensional { // merepresentasi kelas persegi, turunan dari Shape, dan mengimplementasikan TwoDimensional
+    private double side; // menyimpan panjang sisi persegi dengan tipe data double dan modifier private
+
+    public Square() { // default konstruktor
+        setName("Square"); // mengatur nama shape menjadi "Square"
     }
-    
-    public Square(double side) {
-        this();
-        this.side = side;
+
+    public Square(double side) { // konstruktor dengan parameter panjang sisi
+        this(); // memanggil konstruktor default untuk mengatur nama
+        this.side = side; // mengatur panjang sisi dengan nilai parameter
     }
-    
-    @Override
-    public double getArea() {
-        return side * side;
+
+    @Override // meng-override
+    public double getArea() { // method untuk menghitung luas persegi dan bertipe data double
+        return side * side; // mengembalikan nilai luas persegi dengan rumus sisi*sisi
     }
-    
-    @Override
-    public double getPerimeter() {
-        return 4 * side;
+
+    @Override // meng-override
+    public double getPerimeter() { // method untuk menghitung keliling persegi dan bertipe data double
+        return 4 * side; // mengembalikan nilai keliling persegi dengan rumus 4*sisi
     }
-    
-    @Override
-    public void printInfo() {
-        System.out.println("Name            : " + getName());
-        System.out.println("Area            : " + getArea());
-        System.out.println("Perimeter       : " + getPerimeter());
+
+    @Override // meng-override
+    public void printInfo() { // mencetak informasi 
+        System.out.println("Name            : " + getName()); // mencetak nama shape
+        System.out.println("Area            : " + getArea()); // mencetak luas
+        System.out.println("Perimeter       : " + getPerimeter()); // mencetak keliling
     }
 }
