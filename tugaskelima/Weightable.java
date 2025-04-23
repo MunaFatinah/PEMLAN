3f3e3f0dfb9f2979700b09f482de3134fa124253
MunaFatinah <munafatinaha@gmail.com>
2025-04-23 23:30:42 +0700

    Update Weightable.java

--- tugaskelima/Weightable.java
+++ tugaskelima/Weightable.java
@@ -1,6 +1,6 @@
-package praktic.geometry.interfaces;
+package praktic.geometry.interfaces; // package untuk interface 
 
-public interface Weightable {
-    double g = 9.8;
-    double getWeight();
+public interface Weightable { // kelas interface untuk objek yang memiliki berat yakni massa*gravitasi
+    double g = 9.8; // deklarasi nilai gravitasi
+    double getWeight(); // method untuk menghitung berat
 }
