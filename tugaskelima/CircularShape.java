3dce7dbe485ef632120afa115c5d7d65220874d0
MunaFatinah <munafatinaha@gmail.com>
2025-04-23 23:27:52 +0700

    Update CircularShape.java

--- tugaskelima/CircularShape.java
+++ tugaskelima/CircularShape.java
@@ -1,17 +1,17 @@
-package praktic.geometry.bases;
+package praktic.geometry.bases; // merupakan package yang diberi nama bases
 
-public abstract class CircularShape extends Shape {
-    private double radius;
-    protected static final int PI_NUMERATOR = 22;
-    protected static final int PI_DENOMINATOR = 7;
+public abstract class CircularShape extends Shape { // kelas abstrak sebagai dasar bentuk melingkar seperti lingkaran
+    private double radius; // untuk menyimpan jari-jari dan dibuat dengan modifier private
+    protected static final int PI_NUMERATOR = 22; // merupakan pembilang konstanta pi dan bersifat final
+    protected static final int PI_DENOMINATOR = 7; // merupakan penyebut konstanta pi dan bersifat final
 
-    public CircularShape() {}
+    public CircularShape() {} // default constructor
 
-    public void setRadius(double inputRadius) {
-        this.radius = inputRadius;
+    public void setRadius(double inputRadius) { // merupakan setter untuk mengatur nilai radius 
+        this.radius = inputRadius; // mengatur nilai radius dari input
     }
 
-    public double getRadius() {
-        return radius;
+    public double getRadius() { // untuk mendapatkan nilai radius 
+        return radius; // mengembalikan nilai radius
     }
 }
