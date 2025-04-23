cc218699ff1cef6a7f9e0a8f9dc3cdf6aee52d63
MunaFatinah <munafatinaha@gmail.com>
2025-04-23 23:30:18 +0700

    Update TwoDimensional.java

--- tugaskelima/TwoDimensional.java
+++ tugaskelima/TwoDimensional.java
@@ -1,6 +1,6 @@
-package praktic.geometry.interfaces;
+package praktic.geometry.interfaces; // package untuk interface
 
-public interface TwoDimensional {
-    double getArea();
-    double getPerimeter();
+public interface TwoDimensional { // kelas interface bentuk 2d
+    double getArea(); // method untuk menghitung luas
+    double getPerimeter(); // method untuk menghitung keliling
 }
