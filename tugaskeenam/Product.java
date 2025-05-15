1dc7b55a6a5f13af85b70d3da2a714c8aa30c303
MunaFatinah <munafatinaha@gmail.com>
2025-05-15 19:49:07 +0700

    Update Product.java

--- tugaskeenam/Product.java
+++ tugaskeenam/Product.java
@@ -1,42 +1,49 @@
 package Marketplace; // Kelas ini berada dalam package bernama "Marketplace"
 
-public class Product<T extends Comparable<T>> implements Comparable<Product<T>> { // Merupakan kelas generik Product dengan parameter T yang g
-    private int id;
-    private String name;
-    private T category;
-    private double price;
+public class Product<T extends Comparable<T>> implements Comparable<Product<T>> { // Merupakan kelas generik Product dengan kategori (T) dan harus bisa dibandingkan 
+    private int id; // untuk menyimpan id produk, bersifat private, dan tipe datanya int
+    private String name; // untuk menyimpan nama produk bersifat private dan bertipe data String
+    private T category; // untuk menyimmpan kategori produk, merupakan tipe generik T
+    private double price; // untuk menyimpan harga produk dengan tipe data double dan bersifat private
 
+    // merupakan onstruktor untuk menginisialisasi semua atribut
     public Product(int id, String name, T category, double price) {
-        this.id = id;
-        this.name = name;
-        this.category = category;
-        this.price = price;
+        this.id = id; // merujuk pada id
+        this.name = name; // merujuk pada nama
+        this.category = category; /// merujuk pada kategori 
+        this.price = price; // merujuk pada harga
     }
 
+    // merupakan method getter untuk mengambil ID produk
     public int getId() { 
         return id; 
     }
 
+    // method untuk mengambil nama produk
     public String getName() { 
         return name; 
     }
 
+    // method untuk mengambil kategori produk
     public T getCategory() { 
         return category; 
     }
 
+    // method untuk mengambil harga produk
     public double getPrice() { 
         return price; 
     }
 
-
+    // method untuk membandingkan produk berdasarkan kategori
     @Override
     public int compareTo(Product<T> other) {
-        return this.category.compareTo(other.category);
+        return this.category.compareTo(other.category); // membandingkan berdasarkan kategori
     }
 
+    // nethod untuk mengubah data produk ke format string 
     @Override
     public String toString() {
         return "ID: " + id + ", Name: " + name + ", Category: " + category + ", Price: " + price;
     }
 }
+
